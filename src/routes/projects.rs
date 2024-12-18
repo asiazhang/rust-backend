15eaefd371d4f5a68000aefcc93f1cb69d233ded
pinhenzhang <pinhenzhang@tencent.com>
2024-12-18 18:00:04 +0800

    支持projects的模糊搜索和分页查询

--- src/routes/projects.rs
+++ src/routes/projects.rs
@@ -1,13 +1,13 @@
 use crate::models::app::AppState;
 use crate::models::common::Reply;
 use crate::models::common::ReplyList;
+use crate::models::err::AppError;
 use crate::models::projects::{ProjectCreate, ProjectInfo, ProjectSearch};
 use anyhow::Result;
 use axum::extract::State;
 use axum::Json;
 use std::sync::Arc;
 use tracing::debug;
-use crate::models::err::AppError;
 
 /// 根据查询参数搜索项目
 ///
@@ -26,17 +26,43 @@ pub async fn find_projects(
 ) -> Result<Json<ReplyList<ProjectInfo>>, AppError> {
     debug!("Searching projects {:#?}", search);
 
-    let projects = sqlx::query_as!(
-        ProjectInfo,
+    let name = search.project_name.clone();
+    let offset = (search.page_query.page_index.saturating_sub(1)) * search.page_query.page_size;
+    let rows = sqlx::query!(
         r#"
-    select id, project_name from hm.projects
-    "#
+WITH filtered_projects AS (SELECT id,
+                                  project_name,
+                                  COUNT(*) OVER () as total_count
+                           FROM hm.projects
+                           WHERE (COALESCE($1, '') = '' OR project_name LIKE $2)
+                           LIMIT $3 OFFSET $4)
+SELECT id,
+       project_name,
+       total_count
+FROM filtered_projects;
+    "#,
+        name.unwrap_or("".to_string()),
+        search
+            .project_name
+            .map(|n| format!("%{}%", n))
+            .unwrap_or_default(),
+        search.page_query.page_size as i64,
+        offset as i64,
     )
     .fetch_all(&state.db_pool)
     .await?;
 
+    let total = rows.first().and_then(|r| r.total_count).unwrap_or( 0) as u32;
+    let projects = rows
+        .into_iter()
+        .map(|r| ProjectInfo {
+            id: r.id,
+            project_name: r.project_name,
+        })
+        .collect();
+
     Ok(Json(ReplyList {
-        total: projects.len() as u32,
+        total,
         data: projects,
         page_size: search.page_query.page_size,
         page_index: search.page_query.page_index,
