4456a3cda00c28534d2f1a2c309504909a016475
pinhenzhang <pinhenzhang@tencent.com>
2024-12-26 20:31:19 +0800

    add todo

--- src/crons/mod.rs
+++ src/crons/mod.rs
@@ -3,5 +3,7 @@ use tracing::info;
 
 pub async fn start_cron_tasks(_rx: tokio::sync::watch::Receiver<bool>) -> Result<()> {
     info!("starting cron tasks...");
+    
+    // TODO: 增加redis消息再平衡，避免部分消息一直不被处理
     Ok(())
 }
